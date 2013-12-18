fd40f3e80c5c6c58f3686f61fbb7b771909d1496
wocin <925511765@qq.com>
2013-12-18 14:53:15 +0800

    Update cut-nginx-log.sh

--- cut-nginx-log.sh
+++ cut-nginx-log.sh
@@ -7,3 +7,9 @@ log_path="/usr/local/nginx/logs/"
 mkdir -p ${log_path}$(date -d "last-day" +'%Y')/$(date -d "last-day" +'%m')/
 mv ${log_path}access.log ${log_path}$(date -d "last-day" +'%Y')/$(date -d "last-day" +'%m')/access_$(date -d "last-day" +'%Y%m%d').log
 service nginx restart
+#-------------------I am boring line------------------------------------
+#date -d yesterday +'%Y%m%d'
+#date -d last-day +'%Y%m%d'
+#date -d "1 days ago" +'%Y%m%d'
+#date -d "next-day" +'%Y%m%d'
+#date -d last-month +'%Y%m%d'
