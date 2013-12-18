9b8144b906081d27f5d6d2384d89647d3a534a68
wocin <925511765@qq.com>
2013-12-18 14:50:14 +0800

    Update cut-nginx-log.sh

--- cut-nginx-log.sh
+++ cut-nginx-log.sh
@@ -4,6 +4,6 @@
 #-------------------I am boring line------------------------------------
 log_path="/usr/local/nginx/logs/"
 
-mkdir -p ${log_path}$(date -d "yesterday" +'%Y')/$(date -d "yesterday" +'%m')/
-mv ${log_path}access.log ${log_path}$(date -d "yesterday" +'%Y')/$(date -d "yesterday" +'%m')/access_$(date -d "yesterday" +'%Y%m%d').log
+mkdir -p ${log_path}$(date -d "last-day" +'%Y')/$(date -d "last-day" +'%m')/
+mv ${log_path}access.log ${log_path}$(date -d "last-day" +'%Y')/$(date -d "last-day" +'%m')/access_$(date -d "last-day" +'%Y%m%d').log
 service nginx restart
