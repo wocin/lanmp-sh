d578458084d76d074cc4486caf6fd6299d1e07d1
wocin <925511765@qq.com>
2013-12-18 15:35:49 +0800

    Graceful Restart nginx

--- cut-nginx-log.sh
+++ cut-nginx-log.sh
@@ -3,10 +3,12 @@
 #Email ---
 #-------------------I am boring line------------------------------------
 log_path="/usr/local/nginx/logs/"
+nginx_pid="/usr/local/nginx/logs/nginx.pid"
 
 mkdir -p ${log_path}$(date -d "last-day" +'%Y')/$(date -d "last-day" +'%m')/
 mv ${log_path}access.log ${log_path}$(date -d "last-day" +'%Y')/$(date -d "last-day" +'%m')/access_$(date -d "last-day" +'%Y%m%d').log
-service nginx restart
+#service nginx restart
+kill -HUP `cat $nginx_pid`
 #-------------------I am boring line------------------------------------
 #date -d yesterday +'%Y%m%d'
 #date -d last-day +'%Y%m%d'
