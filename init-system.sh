8e7288aed9dbaedafb200a1dc795c721c101a627
wocin <925511765@qq.com>
2014-01-14 15:04:26 +0800

    Update init-system.sh
    change glib2 to glib

--- init-system.sh
+++ init-system.sh
@@ -71,7 +71,7 @@ export HISTTIMEFORMAT
 #yum install software
 yum -y install gcc gcc-c++ autoconf make cmake libjpeg \
 libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 \
-libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel \
+libxml2-devel zlib zlib-devel glibc glibc-devel glib glib-devel \
 bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel \
 e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel \
 gd gd2 gd-devel gd2-devel libevent libevent-devel libtool httpd-tools net-snmp net-snmp-devel perl-DBI \
